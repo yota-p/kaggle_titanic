f48f9db5533fa7f2ec12b048c501777ea4325c53
yota-p <930713yh@gmail.com>
2021-03-04 14:55:50 +0000

    modified params

--- src/train_v1/run001_debug.sh
+++ src/train_v1/run001_debug.sh
@@ -3,7 +3,5 @@
 python src/train_v1/train_v1.py \
 run=run001 \
 experiment.tags.exec=dev \
-model.model_param.n_estimators=2 \
-model.model_param.max_depth=2 \
 model.model_param.tree_method=auto \
 $1
